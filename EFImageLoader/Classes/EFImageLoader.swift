d34653b449a2c91120488e19fbd8ab2998d7af2e
ezefranca <ezequiel.ifsp@gmail.com>
2017-02-28 20:54:42 -0300

    remove unused code

--- EFImageLoader/Classes/EFImageLoader.swift
+++ EFImageLoader/Classes/EFImageLoader.swift
@@ -108,59 +108,16 @@ public class EFImageLoader : UIView {
     
     func startAnimation(uiView: UIView) {
         self.stopAnimating(uiView: uiView)
-        
-        self.stopAnimating(uiView: uiView)
         self.isUserInteractionEnabled = false
-        
-        //viewBackground = UIView(frame: CGRect(x: 0, y: -20, width: screenSize.width, height: screenSize.height + 20))
-        
-        //viewBackground?.backgroundColor = UIColor.black
         viewBackground.alpha = self.backgroundAlpha
         viewBackground.backgroundColor = self.backgroundFadeColor
         imageView.animationImages = self.loadingImages
         imageView.animationDuration = self.animationDuration
-        
         baseView.addSubview(imageView)
         uiView.addSubview(viewBackground)
         uiView.addSubview(baseView)
-        
         imageView.startAnimating()
-//        self.isUserInteractionEnabled = false
-//        print(self.loadingImages.count)
-//        if self.loadingImages != nil {
-//            baseView.addSubview(imageView)
-//            self.addSubview(viewBackground)
-//            self.addSubview(baseView)
-//            imageView.startAnimating()
-//        }
     }
-    
-    
-    
-    //    func startAnimation(uiView: UIView) {
-    //        self.stopAnimating(uiView: uiView)
-    //        self.isUserInteractionEnabled = false
-    //
-    //        viewBackground = UIView(frame: CGRect(x: 0, y: -20, width: screenSize.width, height: screenSize.height + 20))
-    //
-    //        //viewBackground?.backgroundColor = UIColor.black
-    //        viewBackground?.backgroundColor = UIColor.white
-    //        viewBackground?.alpha = 0.2
-    //
-    //        baseView = UIView(frame: CGRect(x: screenSize.width/4, y: screenSize.height/4, width: screenSize.width/4, height: screenSize.height/4))
-    //
-    //        imageView = UIImageView(frame: CGRect(x: (baseView?.frame.width)! - 40, y: (baseView?.frame.height)! - 40, width: 80, height: 80))
-    //
-    //        imageView?.animationImages = loadingImages
-    //        imageView?.animationDuration = 0.9
-    //        baseView?.addSubview(imageView!)
-    //
-    //        uiView.addSubview(viewBackground!)
-    //        uiView.addSubview(baseView!)
-    //
-    //        imageView?.startAnimating()
-    //
-    //    }
     
     func stopAnimating(uiView: UIView) {
         self.imageView.stopAnimating()
@@ -171,111 +128,3 @@ public class EFImageLoader : UIView {
     }
 }
 
-// class EFImageLoader : UIView {
-//
-//    private static let screenSize: CGRect = UIScreen.main.bounds
-//    private static let statusSize = UIApplication.shared.statusBarFrame.height
-//
-//    //MARK: Shared Instance
-//
-//     static let shared : EFImageLoader = {
-//        let instance = EFImageLoader()
-//        return instance
-//    }()
-//
-//    //MARK: Local Variable
-//
-//    var imageView:UIImageView
-//    var baseView:UIView
-//    var viewBackground:UIView
-//    var rootView:UIView
-//
-//     var loadingImages:[UIImage] = []
-//
-//     var animationDuration:TimeInterval
-//
-//    //MARK: Init
-//
-//     convenience init() {
-//        self.init(view:UIView(), imagesFrames:[], backgroundAlpha:0.2, animationDuration:0.9)
-//    }
-//
-//    required  init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    //MARK: Init Properties
-//
-//     init(view: UIView, imagesFrames:[UIImage], backgroundAlpha:CGFloat = 0.2, animationDuration:TimeInterval) {
-//
-//        if imagesFrames == nil {
-//            fatalError("you need to pass an array of images")
-//        }
-//
-//        self.rootView = view
-//        self.animationDuration = animationDuration
-//
-//        self.viewBackground = UIView(frame: CGRect(x: 0, y: -EFImageLoader.statusSize, width: EFImageLoader.screenSize.width, height: EFImageLoader.screenSize.height + EFImageLoader.statusSize))
-//
-//        self.baseView = UIView(frame: CGRect(x: EFImageLoader.screenSize.width/4, y: EFImageLoader.screenSize.height/4, width: EFImageLoader.screenSize.width/4, height: EFImageLoader.screenSize.height/4))
-//
-//        self.imageView = UIImageView(frame: CGRect(x: (baseView.frame.width) - 40, y: (baseView.frame.height) - 40, width: 80, height: 80))
-//
-//
-//        self.loadingImages = imagesFrames
-//        self.viewBackground.backgroundColor = UIColor.darkGray
-//        self.viewBackground.alpha = backgroundAlpha
-//
-//        self.imageView.animationImages = loadingImages
-//        self.imageView.animationDuration = self.animationDuration
-//
-//        self.baseView.addSubview(imageView)
-//        rootView.addSubview(viewBackground)
-//        rootView.addSubview(baseView)
-//
-//        super.init(frame: UIScreen.main.bounds)
-//    }
-//
-//    func setup(view: UIView?, imagesFrames:[UIImage], backgroundAlpha:CGFloat = 0.2, animationDuration:TimeInterval) {
-//
-//        self.animationDuration = animationDuration
-//
-//        self.viewBackground = UIView(frame: CGRect(x: 0, y: -EFImageLoader.statusSize, width: EFImageLoader.screenSize.width, height: EFImageLoader.screenSize.height + EFImageLoader.statusSize))
-//
-//        self.baseView = UIView(frame: CGRect(x: EFImageLoader.screenSize.width/4, y: EFImageLoader.screenSize.height/4, width: EFImageLoader.screenSize.width/4, height: EFImageLoader.screenSize.height/4))
-//
-//        self.imageView = UIImageView(frame: CGRect(x: (baseView.frame.width) - 40, y: (baseView.frame.height) - 40, width: 80, height: 80))
-//
-//
-//        self.loadingImages = imagesFrames
-//        self.viewBackground.backgroundColor = UIColor.darkGray
-//        self.viewBackground.alpha = backgroundAlpha
-//
-//        self.imageView.animationImages = loadingImages
-//        self.imageView.animationDuration = self.animationDuration
-//        self.baseView.addSubview(imageView)
-//
-//        view?.addSubview(viewBackground)
-//        view?.addSubview(baseView)
-//    }
-//
-//     func startAnimation() {
-//        self.stopAnimating()
-//        self.isUserInteractionEnabled = false
-//        print(self.loadingImages.count)
-//        if self.loadingImages != nil {
-//            baseView.addSubview(imageView)
-//            rootView.addSubview(viewBackground)
-//            rootView.addSubview(baseView)
-//            imageView.startAnimating()
-//        }
-//    }
-//
-//     func stopAnimating() {
-//        self.imageView.stopAnimating()
-//        self.imageView.removeFromSuperview()
-//        self.baseView.removeFromSuperview()
-//        self.viewBackground.removeFromSuperview()
-//        self.isUserInteractionEnabled = true
-//    }
-//}
